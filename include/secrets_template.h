4c89937176d0082acab48151b2ef7bfde288c95f
Cesar Ramirez <cesar2020uni@gmail.com>
2026-01-21 20:20:24 -0500

    docs(secrets): add ThingsBoard MQTT config template

--- include/secrets_template.h
+++ include/secrets_template.h
@@ -2,3 +2,9 @@
 
 #define WIFI_SSID     "TU_SSID"
 #define WIFI_PASSWORD "TU_PASSWORD"
+
+// --- ThingsBoard MQTT ---
+#define TB_SERVER "demo.thingsboard.io"
+#define TB_PORT   1883
+#define TB_TOKEN  "YOUR_DEVICE_TOKEN"
+
