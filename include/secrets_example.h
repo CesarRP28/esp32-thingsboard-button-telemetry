fc660a63e74ec50fafd599977043f0331b1d8c70
Cesar Ramirez <cesar2020uni@gmail.com>
2026-01-23 00:14:57 -0500

    docs(secrets): add secrets example and remove old template

---
+++ include/secrets_example.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// -------- WiFi --------
+#define WIFI_SSID  "YOUR_WIFI_SSID"
+#define WIFI_PASS  "YOUR_WIFI_PASSWORD"
+
+// -------- ThingsBoard MQTT --------
+#define TB_HOST    "demo.thingsboard.io"
+#define TB_PORT    1883
+#define TB_TOKEN   "YOUR_THINGSBOARD_DEVICE_TOKEN"
